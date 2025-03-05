8350dcf0bfa907cf74095a084b49dcb1a7238ba7
jyzhang <peakfind@126.com>
2025-03-05 21:19:49 +0100

    update using and export

--- src/Nmrc.jl
+++ src/Nmrc.jl
@@ -3,10 +3,17 @@ module Nmrc
 using Gmsh
 using Ferrite, FerriteGmsh
 using SparseArrays
+using KrylovKit: svdsolve
+
+export get_scaling_factors, compute_scaling
 
 # PML parameters
 include("pml.jl")
+
 # FEM routines
 include("fem/setups.jl")
+
+# Quadratic eigenvalue problem solver
+include("quadeig.jl")
 
 end
