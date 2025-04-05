01c1b8e20a368a3a39e1a4535207b472584bd469
jyzhang <peakfind@126.com>
2025-04-05 13:23:03 +0200

    add export

--- src/Nmrc.jl
+++ src/Nmrc.jl
@@ -1,7 +1,8 @@
 module Nmrc
 
 using Gmsh
-using Ferrite, FerriteGmsh
+using Ferrite
+using FerriteGmsh: togrid
 using LinearAlgebra
 using SparseArrays
 using KrylovKit: svdsolve
@@ -10,11 +11,22 @@ using KrylovKit: svdsolve
 include("pml.jl")
 export PML, get_width, coord_transform
 
+# Incident plane wave
+include("incident.jl")
+export Incident, get_alpha, get_beta, beta_n
+
 # FEM routines
 include("fem/setups.jl")
 export setup_grid, setup_fevs, setup_dofs
 export setup_bdcs, allocate_matrices
 export assemble_A2, assemble_A1, assemble_A0, apply_all_bds!
+
+include("fem/dtn.jl")
+export periodic_cell
+export setup_vals
+export dofs_on_dtn, setup_bcs, allocate_stiff_matrix
+export assemble_A, assemble_load, assemble_tbc
+export sub_preserve_structure
 
 # Quadratic eigenvalue problem solver
 include("quadeig.jl")
