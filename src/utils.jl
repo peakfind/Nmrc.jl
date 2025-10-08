fb49d78f2e4b1db61579e89bdc9926b175bf48b2
jyzhang <peakfind@126.com>
2025-10-08 11:04:04 +0200

    Add functions for computing integrals

--- src/utils.jl
+++ src/utils.jl
@@ -28,3 +28,70 @@ end
 
 # Convenience method for other number types
 my_sqrt(z::Complex) = my_sqrt(float(z))
+
+"""
+    integral_uv(cv::CellValues, dh::DofHandler, u, v)
+
+Compute the integration of the production of function ``u``` and ``v``. Here 
+``u`` and ``v`` are vectors indexed by DoFs. Normally, they are results obtained 
+by FEM.
+"""
+function integral_uv(cv::CellValues, dh::DofHandler, u, v)
+    # Check if the elements in u and v have the same type
+    (eltype(u) == eltype(v)) || throw(ArgumentError("elements in u and v should have the same type!")) 
+    
+    rst = zero(eltype(u))
+    
+    for cell in CellIterator(dh)
+        # Reinitialize cellvalues for this cell
+        reinit!(cv, cell)
+        
+        # Get the corresponding cell id of the current cell cache
+        ci = cellid(cell)
+        
+        # Get the DoFs of the cell `ci`
+        cd = celldofs(dh, ci)
+
+        for qp in 1:getnquadpoints(cv)
+            dx = getdetJdV(cv, qp)
+            u_qp = function_value(cv, qp, u, cd)
+            v_qp = function_value(cv, qp, v, cd)
+            rst += u_qp * v_qp * dx
+        end
+    end
+    
+    return rst
+end
+
+"""
+    integral_∂₁uv(cv::CellValues, dh::DofHandler, u, v)
+
+Compute the integration of the product of ``\\partial_{1}u`` (the partial derivative 
+of function ``u``) and ``v``. Here ``u`` and ``v`` are vectors indexed by DoFs. Normally, 
+they are results obtained by FEM.
+"""
+function integral_∂₁uv(cv::CellValues, dh::DofHandler, u, v)
+    # Check if the elements in u and v have the same type
+    (eltype(u) == eltype(v)) || throw(ArgumentError("elements in u and v should have the same type!"))  
+
+    rst = zero(eltype(u))
+    
+    for cell in CellIterator(dh)
+        # Reinitialize cellvalues for this cell
+        reinit!(cv, cell)
+        
+        # Get the corresponding cell id of the current cell cache
+        ci = cellid(cell)
+        # Get the DoFs of the cell `ci`
+        cd = celldofs(dh, ci)
+
+        for qp in 1:getnquadpoints(cv)
+            dx = getdetJdV(cv, qp)
+            ∂₁u_qp = function_gradient(cv, qp, u, cd)[1]
+            v_qp = function_value(cv, qp, v, cd)
+            rst += ∂₁u_qp * v_qp * dx
+        end
+    end
+    
+    return rst
+end
